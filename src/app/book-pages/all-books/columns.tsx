0289161b179f0d792c5cfdb9bf8f0626bea4e616
21-COMPA36-25 Steve Fernandes <1032210418@tcetmumbai.in>
2025-03-20 16:24:54 +0530

    chore: clearing logs

--- src/app/book-pages/all-books/columns.tsx
+++ src/app/book-pages/all-books/columns.tsx
@@ -6,11 +6,25 @@ import { useRouter } from "next/navigation";
 import images from "@/images/index";
 import { formatDate } from '../hooks/formatDate';
 
+// TODO: change Any
+const BookTitleCell = ({book} : any) => {
+  const router = useRouter();
+  return <div
+  className="relative group cursor-pointer"
+  onClick={() => router.push(`/book-pages/book-details?book_uuid=${book.book_uuid}`)}
+>
+  {book.book_title}
+  <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:flex items-center justify-center bg-gray-800 text-white text-xs rounded-lg px-3 py-1 shadow-md whitespace-nowrap
+    after:content-[''] after:absolute after:top-full after:left-1/2 after:-translate-x-1/2 after:border-4 after:border-transparent after:border-t-gray-800">
+    Book Details
+  </div>
+</div>
+}
+
 export const getBookColumns = (
 
   handleEdit: (book: BookData) => void,
 ): ColumnDef<BookData>[] => {
-  const router = useRouter();
 
   return [
 
@@ -21,16 +35,17 @@ export const getBookColumns = (
       cell: ({ row }) => {
         const book = row.original;
         return (
-          <div
-            className="relative group cursor-pointer"
-            onClick={() => router.push(`/book-pages/book-details?book_uuid=${book.book_uuid}`)}
-          >
-            {book.book_title}
-            <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:flex items-center justify-center bg-gray-800 text-white text-xs rounded-lg px-3 py-1 shadow-md whitespace-nowrap
-              after:content-[''] after:absolute after:top-full after:left-1/2 after:-translate-x-1/2 after:border-4 after:border-transparent after:border-t-gray-800">
-              Book Details
-            </div>
-          </div>
+          <BookTitleCell book={book}/>
+          // <div
+          //   className="relative group cursor-pointer"
+          //   onClick={() => router.push(`/book-pages/book-details?book_uuid=${book.book_uuid}`)}
+          // >
+          //   {book.book_title}
+          //   <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:flex items-center justify-center bg-gray-800 text-white text-xs rounded-lg px-3 py-1 shadow-md whitespace-nowrap
+          //     after:content-[''] after:absolute after:top-full after:left-1/2 after:-translate-x-1/2 after:border-4 after:border-transparent after:border-t-gray-800">
+          //     Book Details
+          //   </div>
+          // </div>
         );
       },
     },
