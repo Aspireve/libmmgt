a96a5eeb9c4f1d2b17a429cb73a9c22451ddb694
21-COMPA36-25 Steve Fernandes <1032210418@tcetmumbai.in>
2025-03-20 16:34:27 +0530

    chore: fixed broken lib name

--- src/components/dashboard/activity-feed.tsx
+++ src/components/dashboard/activity-feed.tsx
@@ -18,6 +18,8 @@ export default function Activities({ refresh }: { refresh: number }) {
   // Ensure data?.data is an array before mapping
   const activityLogs = Array.isArray(data?.data) ? data.data : [];
 
+  console.log({activityLogs})
+
   return (
     <div className="transition-all duration-300 hover:shadow-lg border border-[#AEB1B9] max-w-[90%] h-110 rounded-[10px] bg-[#F3F4F6] ml-10 mt-5 p-6">
       <h2 className="text-2xl font-semibold mb-4">Activities</h2>
@@ -26,7 +28,7 @@ export default function Activities({ refresh }: { refresh: number }) {
         <ActivityLog
           key={`activity-${idx}`}
           type={item?.action as ActivityType}
-          title={item?.new_booktitle?.[0]?.book_title || "Unknown Title"}
+          title={item?.new_book_title?.book_title || "Unknown Title"}
           studentName={item?.student_name || "Unknown Student"}
           time={item?.created_at || "Unknown Time"}
           isLoading={isLoading}
