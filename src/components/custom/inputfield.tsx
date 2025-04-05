d94027c6114d396a74857e673dc3812f763d59f1
Tejas Kelaskar <90203396+Rossi16@users.noreply.github.com>
2025-04-05 11:54:50 +0530

    UI changes in activites

--- src/components/custom/inputfield.tsx
+++ src/components/custom/inputfield.tsx
@@ -44,7 +44,7 @@ export const InputField = <T extends FieldValues>({
         <Skeleton className="h-10 w-full animate-pulse bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 bg-[length:200%_100%]" />
       ) : (
         <Input
-          className="text-[#000000] placeholder:text-[#717680] border-[#D5D7DA] rounded-[8px]"
+          className="text-[#000000] placeholder:text-[#717680] border-[#D5D7DA] rounded-[8px] relative"
           type={type}
           min={disablePast ? today : undefined} 
           max={disableFuture ? today : undefined}
